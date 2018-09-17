8384b887bd3fd89927c96221df3b374cdfc2bb69
Eugene Kitsios <eugene.kitsios@gmail.com>
2018-09-17 11:36:01 +0200

    Backend Product

---
+++ src/main/java/com/winstgenerator/backend/BackendProductApplication.java
@@ -0,0 +1,12 @@
+package com.winstgenerator.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProductApplication.class, args);
+	}
+}
---
+++ src/main/java/com/winstgenerator/backend/model/Product.java
@@ -0,0 +1,5 @@
+package com.winstgenerator.backend.model;
+
+public class Product {
+
+}
