4e581ffe46d22dead39927024e527537700736e0
Eugene Kitsios <eugene.kitsios@gmail.com>
2018-09-17 15:12:09 +0200

    V3.0 model Product

--- src/main/java/com/winstgenerator/backend/dao/IProductDao.java
+++ src/main/java/com/winstgenerator/backend/dao/IProductDao.java
@@ -2,9 +2,9 @@ package com.winstgenerator.backend.dao;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+
 
 import com.winstgenerator.backend.model.Product;
 
